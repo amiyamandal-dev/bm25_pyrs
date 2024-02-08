5de944b1b9ff839297320cf7c046198e87b26c03
Amiya Mandal <amiya19mandal@gmail.com>
2024-02-08 15:12:23 +0530

    code added

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,48 @@ struct BM25Okapi {
     epsilon: f64,
 }
 
+fn calc_idf(b: &mut BM25Okapi, nd: &HashMap<String, f64>) {
+    let mut idf_sum = 0.0;
+    let mut negative_idfs: Vec<_> = Vec::new();
+
+    for (word, freq) in nd.into_iter() {
+        let idf = f64::ln(b.corpus_size - freq + 0.5) - f64::ln(freq + 0.5);
+        b.idf.insert(word.clone(), idf);
+        idf_sum += idf;
+        if idf < 0.0 {
+            negative_idfs.push(word.clone());
+        }
+    }
+    b.average_idf = idf_sum / b.idf.len() as f64;
+    let eps = b.epsilon * b.average_idf;
+    for i in negative_idfs.iter() {
+        b.idf.insert(i.clone(), eps);
+    }
+}
+
+fn initialize(b: &mut BM25Okapi, corpus: &Vec<Vec<String>>) -> HashMap<String, f64> {
+    let mut nd: HashMap<String, f64> = HashMap::new();
+    let mut num_doc = 0;
+
+    for doc in corpus.iter() {
+        b.doc_len.push(doc.len());
+        num_doc += doc.len();
+
+        let mut frequencies = HashMap::new();
+        for word in doc.iter() {
+            *frequencies.entry(word.to_string()).or_insert(0) += 1;
+        }
+        b.doc_freqs.push(frequencies.clone());
+        for (word, _) in frequencies.into_iter() {
+            *nd.entry(word).or_insert(0.0) += 1.0;
+        }
+        b.corpus_size += 1.0;
+    }
+
+    b.avgdl = num_doc as f64 / b.corpus_size;
+    nd
+}
+
 #[pymethods]
 impl BM25Okapi {
     #[new]
@@ -43,54 +85,12 @@ impl BM25Okapi {
             average_idf: 0.0,
             epsilon,
         };
-        let nd = bm25.initialize(corpus);
-        bm25._calc_idf(nd);
+        let nd = initialize(&mut bm25, &corpus);
+        calc_idf(&mut bm25, &nd);
         bm25
     }
 
-    fn initialize(&mut self, corpus: Vec<Vec<String>>) -> HashMap<String, f64> {
-        let mut nd: HashMap<String, f64> = HashMap::new();
-        let mut num_doc = 0;
-
-        for doc in corpus.iter() {
-            self.doc_len.push(doc.len());
-            num_doc += doc.len();
-
-            let mut frequencies = HashMap::new();
-            for word in doc.iter() {
-                *frequencies.entry(word.to_string()).or_insert(0) += 1;
-            }
-            self.doc_freqs.push(frequencies.clone());
-            for (word, _) in frequencies.into_iter() {
-                *nd.entry(word).or_insert(0.0) += 1.0;
-            }
-            self.corpus_size += 1.0;
-        }
-
-        self.avgdl = num_doc as f64 / self.corpus_size;
-        nd
-    }
-
-    fn _calc_idf(&mut self, nd: HashMap<String, f64>) {
-        let mut idf_sum = 0.0;
-        let mut negative_idfs: Vec<_> = Vec::new();
-
-        for (word, freq) in nd.into_iter() {
-            let idf = f64::ln(self.corpus_size - freq + 0.5) - f64::ln(freq + 0.5);
-            self.idf.insert(word.clone(), idf);
-            idf_sum += idf;
-            if idf < 0.0 {
-                negative_idfs.push(word.clone());
-            }
-        }
-        self.average_idf = idf_sum / self.idf.len() as f64;
-        let eps = self.epsilon * self.average_idf;
-        for i in negative_idfs.iter() {
-            self.idf.insert(i.clone(), eps);
-        }
-    }
-
-    fn get_scores(&mut self, query: Vec<String>)->Vec<f64>{
+    fn get_scores(&mut self, query: Vec<String>) -> Vec<f64> {
         let mut scores: Vec<f64> = vec![0.0; self.corpus_size as usize];
         let doc_len = &self.doc_len;
         for q in query.iter() {
@@ -104,17 +104,20 @@ impl BM25Okapi {
                 Some(t) => *t,
                 None => 0.0,
             };
-            scores.par_iter_mut().enumerate()  // Parallel iteration over scores
+            scores
+                .par_iter_mut()
+                .enumerate() // Parallel iteration over scores
                 .for_each(|(i, score)| {
                     let freq = q_freq[i];
                     let doc_len_i = doc_len[i] as f64;
-                    let bm25_score = idf_score * (freq * (self.k1 + 1.0) / (freq + self.k1 * (1.0 - self.b + self.b * doc_len_i / self.avgdl)));
-                    *score += bm25_score;  // Update the score
+                    let bm25_score = idf_score
+                        * (freq * (self.k1 + 1.0)
+                            / (freq + self.k1 * (1.0 - self.b + self.b * doc_len_i / self.avgdl)));
+                    *score += bm25_score; // Update the score
                 });
         }
         scores
     }
-
 }
 
 #[pyfunction]
