422708dc292229ca013ddce9f1a04cb49c070cfd
amiya <amiya19mandal@gmail.com>
2025-09-30 14:40:44 +0530

    code updated

--- python/bm25_rs/__init__.py
+++ python/bm25_rs/__init__.py
@@ -8,11 +8,11 @@ from ._bm25_rs import BM25Okapi, BM25Plus, BM25L
 
 __version__ = "0.1.0"
 __author__ = "BM25-RS Team"
-__email__ = "maintainers@example.com"
+__email__ = "amiya8mandal@gmail.com"
 
 __all__ = [
     "BM25Okapi",
-    "BM25Plus", 
+    "BM25Plus",
     "BM25L",
 ]
 
@@ -22,6 +22,6 @@ from .benchmarks import benchmark_bm25
 
 __all__.extend([
     "tokenize_text",
-    "preprocess_corpus", 
+    "preprocess_corpus",
     "benchmark_bm25",
 ])
